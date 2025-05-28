6eeb9de99987c8ba32d9f5ebc56fa0b1501ba922
wajahat ali <wajiali744@gmail.com>
2025-05-28 12:03:24 +0500

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h1 className='mb-4'>
+      Hello world!
+    </h1>
+    <p1 className='bg-red-300 txt-green p-3'>
+      wajahat ali
+    </p1>
+    </>
+  )
+}
+
+export default App
